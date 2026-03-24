82b3e83e1437cae8fd3c3c6ea1675c78c6fade31
Sultan Ashraf <sultanashraf.repo@gmail.com>
2026-03-24 15:15:36 +0530

    Refactor star pattern code by uncommenting and modifying the nested loop for clarity

--- variables-datatypes/star-pattern.py
+++ variables-datatypes/star-pattern.py
@@ -1,4 +1,17 @@
+# for i in range(5):
+#     for j in range(i+1):
+#         print("* ", end="")
+#     print() #is used to move to the next line after each row of stars is printed
+    
 for i in range(5):
-    for j in range(i+1):
-        print("* ", end="")
-    #print() is used to move to the next line after each row of stars is printed
+    print((i+1) * "* ")
+    
+#reverse star pattern
+for i in range(5,0,-1):
+    print(i * "* ")
+    
+# for number in range(101):
+#     if number % 2 == 0:
+#         print(f"{number} is even number")
+#     else:
+#         print(f"{number} is odd number")
