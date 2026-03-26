93843e927df66242204e908f3822d6b03ad40596
Sultan Ashraf <sultanashraf.repo@gmail.com>
2026-03-27 00:12:17 +0530

    Refactor even number extraction and add list comprehension for squares

--- variables-datatypes/List-comprehansen.py
+++ variables-datatypes/List-comprehansen.py
@@ -5,3 +5,14 @@ for i in range(len(numbers)):
         even_numbers.append(numbers[i])
 
 print(f"Even numbers are: {even_numbers}")
+
+#------------------List Comprehension(Square)----------------------
+numbers = [1,2,3,4,5,6,7,8,9,10]
+squares = [x*x for x in numbers]
+print(f"Square of numbers are: {squares}")
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+even_numbers = [x for x in numbers if x % 2 == 0]
+print(f"Even numbers are: {even_numbers}")
+
