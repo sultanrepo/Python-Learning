e3f8ba86bf3c811044607b383b1ae3c42753592c
Sultan Ashraf <sultanashraf.repo@gmail.com>
2026-04-09 22:49:11 +0530

    Fix logger info formatting in tuple power calculation

--- variables-datatypes/tuple-assign2.py
+++ variables-datatypes/tuple-assign2.py
@@ -6,4 +6,5 @@ tuple2 = (3,6,4,3)
 power_values = []
 for i in range(len(tuple1)):
     power_values.append(tuple1[i] ** tuple2[i])
-logger.info(tuple(power_values))
+logger.info(tuple(power_values)
+            )
