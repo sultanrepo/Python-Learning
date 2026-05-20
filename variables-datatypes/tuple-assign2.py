6154c0b4621d492bfa88cf94e6d3f3422782f284
Sultan Ashraf <sultanashraf.repo@gmail.com>
2026-05-20 12:17:03 +0530

    Add tuple concatenation and logging for final tuple output

--- variables-datatypes/tuple-assign2.py
+++ variables-datatypes/tuple-assign2.py
@@ -8,3 +8,8 @@ for i in range(len(tuple1)):
     power_values.append(tuple1[i] ** tuple2[i])
 logger.info(tuple(power_values))
 #Done--
+#----------------------------------------------------
+test_tuple = (1,45,2,65,2,32,"True",2,"Manish",True)
+new_tuple = (23,43,54)
+final_tuple = test_tuple + new_tuple
+logger.info(final_tuple)
